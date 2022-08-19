f0251010619f06844c13044ea5456fdcf87d141e
elitechilli <elitechilli96@gmail.com>
2022-08-19 18:49:59 -0300

    Adicionando a funcionalidade de Password Checker Se o usuário digitar um senha menor do quer 6 caracteres ou não digitar nada em ambos inputs o botão de login será desativado

--- instagramClone/assets/js/script.js
+++ instagramClone/assets/js/script.js
@@ -1,9 +1,9 @@
-
+// Slider de photos
 var count = 0
 var photos = ['./assets/img/mockup-1.png',
-        './assets/img/mockup-2.png',
-        './assets/img/mockup-3.png',
-        './assets/img/mockup-4.png']
+    './assets/img/mockup-2.png',
+    './assets/img/mockup-3.png',
+    './assets/img/mockup-4.png']
 
 setInterval(changePhoto, 5000)
 
@@ -17,3 +17,25 @@ function changePhoto() {
     img.src = photos[count]
     count += 1
 }
+
+// Verificador do tamanho da senha
+
+btnChecker()
+document.getElementById('user').addEventListener('input',btnChecker)
+document.getElementById('password').addEventListener('input',btnChecker)
+
+function btnChecker() {
+    let user = document.getElementById('user').value
+    let pass = document.getElementById('password').value
+    let btn = document.getElementById('btnLogIn')
+
+    if (user.length == 0 || pass.length < 6) {
+        btn.disabled = true
+    }else{
+        btn.disabled = false
+    }
+}
+
+
+
+
